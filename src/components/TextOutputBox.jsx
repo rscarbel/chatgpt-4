15811acb7d530c3552524787de2a37f4f6be077c
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-03-29 00:15:22 -0400

    added syntax highlighting

--- src/components/TextOutputBox.jsx
+++ src/components/TextOutputBox.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { parseMarkdown } from "../scripts/parseMarkdown";
+import "highlight.js/styles/github.css";
 
 const formatTime = (dateItem) => {
   const date = new Date(dateItem);
--- src/scripts/parseCodeBlock.js
+++ src/scripts/parseCodeBlock.js
@@ -1,32 +1,52 @@
-export const parseCodeBlock = (html) => {
+import hljs from "highlight.js";
+import "highlight.js/styles/vs2015.css";
+
+const addLineBreaksAndClasses = (html) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, "text/html");
   const codeBlocks = doc.querySelectorAll("code");
   codeBlocks.forEach((codeBlock) => {
+    if (codeBlock.classList.contains("code-small")) return;
     const codeBlockText = codeBlock.innerHTML;
     const componentWords = codeBlockText.split(" ");
-    if (componentWords.length === 1) {
-      codeBlock.classList.add("code-small");
-    }
-    if (componentWords[0] === "javascript") {
-      for (let i = 0; i < componentWords.length; i++) {
-        const word = componentWords[i];
-        if (word.endsWith(";")) {
-          componentWords[i] += "<br>";
-          continue;
-        }
-        if (word === "{") {
-          componentWords[i] += "<br>&nbsp;&nbsp;";
-          continue;
-        }
-        if (word === "}") {
-          componentWords[i] = "<br>&nbsp;&nbsp;}";
-          continue;
-        }
+    for (let i = 0; i < componentWords.length; i++) {
+      const word = componentWords[i];
+      if (word.endsWith(";")) {
+        componentWords[i] += "<br>";
       }
     }
     codeBlock.innerHTML = componentWords.join(" ");
   });
+
+  return doc.body.innerHTML;
+};
+
+export const parseCodeBlock = (html) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const codeBlocks = doc.querySelectorAll("code");
+  let isJavascript = false;
+  codeBlocks.forEach((codeBlock) => {
+    const codeBlockText = codeBlock.innerHTML;
+    const componentWords = codeBlockText.split(" ");
+    const isOneWord = componentWords.length === 1;
+    if (isOneWord) {
+      codeBlock.classList.add("code-small");
+    }
+    if (!isOneWord && componentWords[0] === "javascript") {
+      isJavascript = true;
+    }
+    const modifiedText = componentWords.join(" ");
+    if (isOneWord) {
+      codeBlock.innerHTML = modifiedText;
+    } else {
+      const language = componentWords[0];
+      const code = componentWords.slice(1).join(" ");
+      codeBlock.innerHTML = hljs.highlight(language, code).value;
+    }
+  });
+
+  if (isJavascript) return addLineBreaksAndClasses(doc.body.innerHTML);
 
   return doc.body.innerHTML;
 };
